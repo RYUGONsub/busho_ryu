ec03ba2315796948cf0325ecbff254300ff1d1f9
RYUGON <60171641+RYUGON@users.noreply.github.com>
2021-05-05 22:22:35 +0900

    2021/5/5 22:22
    6

---
+++ js/footer.js
@@ -0,0 +1,4 @@
+javascript:(function()
+{
+    document.write('<div class="footer"><div class="footer-logo">武将リュウ</div><div class="footer-list"><ul><li><a href="https://twitter.com/Busho_RYU">武将リュウ Twitter</a></li><li><a href="https://www.youtube.com/channel/UClAkxTGY4s2b8J-VssVBymw">武将リュウ YouTubeChannel</a></li><li><a href="https://rookie.shonenjump.com/series/X1vJnKYo1t8">マメターズの漫画(全2話)</a></li></ul></div></div>');
+})()
