545183bf5cf1ddc9a76a41ca37e4c2771fca1b4b
RYUGON <60171641+RYUGON@users.noreply.github.com>
2021-05-05 23:42:05 +0900

    2021/5/5 23:42
    9

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
 javascript:(function()
 {
-    document.write('');
+    document.write('<div class="header"><div class="header-logo"><a href="index.html">武将リュウ</a></div><div class="header-list"><ul><li><a href="index.html">ブログ的なやつ</a></li><li><a href="index.html">武将リュウについて</a></li><li><a href="Mameters.html">マメターズ</a></li></ul></div></div>');
 })()
