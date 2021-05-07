59a1082dd2cee4683bd7ceca46740cbe2128c7c5
RYUGON <60171641+RYUGON@users.noreply.github.com>
2021-05-07 19:10:53 +0900

    2021/5/7 19:10
    18

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,4 +1,4 @@
 javascript:(function()
 {
-    document.write('<div class="contact-form"><form method="post" action="xxxxxxxx.php" id="form1" name="form1"><h3 class="section-title" id="info0002">武将リュウへメッセージ</h3><p>メールアドレス（必須）</p><td><input type="email" size="30" name="mail" id="mail"></td><p>お名前</p><td><input type="text" size="20" name="namae" id="namae"></td><p>メールの内容（必須）</p><td><textarea name="naiyou" cols="40" rows="5"></textarea></td><p>※必須項目は必ずご入力ください</p><input class="contact-submit" type="submit" value="送信"></div>');
+    document.write('<div class="contact-form"><form method="post" action="xxxxxxxx.php" id="form1" name="form1"><h3 class="section-title" id="info0002">武将リュウへメッセージ(準備中)</h3><p>メールアドレス（必須）</p><td><input type="email" size="30" name="mail" id="mail"></td><p>お名前</p><td><input type="text" size="20" name="namae" id="namae"></td><p>メールの内容（必須）</p><td><textarea name="naiyou" cols="40" rows="5"></textarea></td><p>※必須項目は必ずご入力ください</p><input class="contact-submit" type="submit" value="送信"></div>');
 })()
