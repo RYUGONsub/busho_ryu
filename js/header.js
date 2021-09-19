3dc8064a5dc84fe96f6559181122f0471ce5f2d9
RYUGON <60171641+BushoRyu@users.noreply.github.com>
2021-09-20 01:28:48 +0900

    2021/9/20
    お知らせ機能大幅追加

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
 javascript:(function()
 {
-    document.write('<div class="header"><div class="header-logo"><a href="index.html">武将リュウ</a></div><div class="header-list"><ul><li><a href="index.html">ブログ的なやつ</a></li><li><a href="index.html">武将リュウについて</a></li><li><a href="Mameters.html">マメターズ</a></li></ul></div></div>');
+    document.write('<div class="header"><div class="header-logo"><a href="index.html">武将リュウ</a></div><div class="header-list"><ul><li><a href="index.html">ブログ的なやつ</a></li><li><a href="index.html">武将リュウについて</a></li><li><a href="Mameters.html">マメターズ</a></li><li><a href="infoTab.html">お知らせ</a></li></ul></div></div>');
 })()
